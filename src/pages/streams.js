39109801f4a960326f1aaebf905c8ab89781f721
Blake Green <blakegreen@msn.com>
2021-02-12 08:05:13 -0800

    tweak the streams

--- src/pages/streams.js
+++ src/pages/streams.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import { graphql } from "gatsby";
+// import { graphql } from "gatsby";
 // import { Link } from "gatsby";
-import moment from "moment";
+// import moment from "moment";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 export default class Streams extends Component {
   render() {
-    const { data } = this.props;
+    // const { data } = this.props;
     return (
       <Layout>
         <SEO
@@ -20,7 +20,7 @@ export default class Streams extends Component {
             <div className="section-head">
               <h1 className="line-heading h2">Streams</h1>
             </div>
-            <ul className="blogs-list">
+            {/* <ul className="blogs-list">
               {data.allContentfulStreams.edges.map((item, index) => {
                 return (
                   <li key={index} className="item">
@@ -37,7 +37,7 @@ export default class Streams extends Component {
                   </li>
                 );
               })}
-            </ul>
+            </ul> */}
           </div>
         </div>
       </Layout>
@@ -46,19 +46,19 @@ export default class Streams extends Component {
 }
 // https://www.gatsbyjs.com/docs/data-fetching/#fetching-data-at-client-side-runtime
 // https://www.youtube.com/watch?v=8YWrmZoUYGs&t=136s
-export const pageQuery = graphql`
-  query StreamsQuery {
-    allContentfulProjects(
-      sort: { fields: [date], order: DESC }
-    ){
-      edges {
-        node {
-          title
-          slug
-          createdAt
-          date(formatString: "DD MMMM, YYYY")
-        }
-      }
-    }
-  }
-`;
+// export const pageQuery = graphql`
+//   query StreamsQuery {
+//     allContentfulProjects(
+//       sort: { fields: [date], order: DESC }
+//     ){
+//       edges {
+//         node {
+//           title
+//           slug
+//           createdAt
+//           date(formatString: "DD MMMM, YYYY")
+//         }
+//       }
+//     }
+//   }
+// `;
